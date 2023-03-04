e515476952df51e84314040a76fe319f9e5ab416
dadonov <dadonov.igor@gmail.com>
2023-03-04 17:12:12 +0400

    feat: add solution for guessing game task

---
+++ guessing-game/src/guessing-game.js
@@ -0,0 +1,23 @@
+class GuessingGame {
+  constructor() {}
+
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
+
+  guess() {
+    this.mid = Math.ceil((this.min + this.max) / 2);
+    return this.mid;
+  }
+
+  lower() {
+    this.max = this.mid;
+  }
+
+  greater() {
+    this.min = this.mid;
+  }
+}
+
+module.exports = GuessingGame;
